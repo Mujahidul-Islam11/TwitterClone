8dfecceb5c0c850cfabcc534939024a06aacd772
Mohammad Dinar <web.dev.dinar@gmail.com>
2025-03-11 12:45:46 +0600

    Added user following unfollowing controller

--- Backend/controller/userController.js
+++ Backend/controller/userController.js
@@ -15,7 +15,33 @@ const getUserProfile = async (req, res) => {
 
 const followUnfollow = async (req, res) => {
   try {
-    
+    const { id } = req.params;
+    const userModify = await User.findById(id);
+    const currentUser = await User.findOne(req.user._id);
+
+    if (id === req.user._id.toString()) {
+      return res
+        .status(400)
+        .json({ error: "You can't follow/unfollow yourself" });
+    }
+
+    if (!userModify || !currentUser) {
+      return res.status(400).json({ error: "User Not Found" });
+    }
+
+    const isFollowing = currentUser.following.includes(id);
+
+    if (isFollowing) {
+      // unfollow the user
+      await User.findByIdAndUpdate(id, { $pull: { followers: req.user._id } });
+      await User.findByIdAndUpdate(req.user._id, { $pull: { following: id } });
+      res.status(200).json({ message: "User Unfollowed Successfully" });
+    } else {
+      // folow the user
+      await User.findByIdAndUpdate(id, { $push: { followers: req.user._id } });
+      await User.findByIdAndUpdate(req.user._id, { $push: { following: id } });
+      res.status(200).json({ message: "User Followed Successfully" });
+    }
   } catch (err) {
     res.status(500).json({ error: "Internal Server Error" });
   }
