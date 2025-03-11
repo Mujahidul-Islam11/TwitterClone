2e7f29d4e1b69ebf2ca2cdbdb848da18e462bf40
Mohammad Dinar <web.dev.dinar@gmail.com>
2025-03-11 10:37:30 +0600

    Profile getting controller added

--- Backend/controller/userController.js
+++ Backend/controller/userController.js
@@ -1,5 +1,17 @@
+const User = require("../models/user.model");
 
-const getUserProfile = async (req, res) => {};
+const getUserProfile = async (req, res) => {
+  try {
+    const { username } = req.params;
+    const user = await User.findOne({username}).select("-password");
+    if(!user){
+        return res.status(404).json({error: "User Not Found"});
+    }
+    res.status(200).json(user);
+  } catch (err) {
+    res.status(500).json({error: err.message});
+  }
+};
 
 const followUnfollow = async (req, res) => {};
 
