354586d8a813f4bd7c98f8cb24c5b2819a5f458c
Mohammad Dinar <web.dev.dinar@gmail.com>
2025-03-15 00:08:47 +0600

    UpdateProfile controller created and updated user data

--- Backend/controller/userController.js
+++ Backend/controller/userController.js
@@ -80,17 +80,71 @@ const suggestedUsers = async (req, res) => {
     suggestedUsers.forEach((user) => (user.password = null));
 
     res.status(200).json(suggestedUsers);
-
   } catch (err) {
     res.status(500).json({ error: "Internal Server Error" });
   }
 };
 
-const updateProfile = async (req, res) => {};
+const updateProfile = async (req, res) => {
+  const { email, username, fullName, bio, link, newPassword, currentPassword } = req.body;
+  let { profileImg, coverImg } = req.body;
+
+  try {
+    const userId = req.user._id;
+    const user = await User.findById(userId);
+    if(!user){
+      return res.status(404).json({error: "User Not Found"});
+    }
+    if((!currentPassword && newPassword) || (!newPassword && currentPassword)){
+      return res.status(400).json({error: "Please provide both current and new password"})
+    }
+
+    if(currentPassword && newPassword){
+      const isMatch = await bcrypt.compare(currentPassword, user.password);
+
+      if(!isMatch){
+        return res.status(400).json({error: "Current password is incorrect"})
+      }
+      if(newPassword.length <6){
+        return res.status(400).json({error: "Password must be at least 6 characters long"})
+      }
+      
+      const salt = await bcrypt.genSalt(10);
+      user.password = await bcrypt.hash(newPassword, salt);
+    }
+
+    if(profileImg){
+      if(user.profileImg){
+        await cloudinary.uploader.destroy().user.profileImg.split('/').pop().split('.')[0]
+      }
+      const profileImgRes = await cloudinary.uploader.upload(profileImg);
+      profileImg = profileImgRes.secure_url;
+    }
+    if(coverImg){
+      if(user.coverImg){
+        await cloudinary.uploader.destroy().user.coverImg.split('/').pop().split('.')[0]
+      }
+      const coverImgRes = await cloudinary.uploader.upload(coverImg);
+      coverImg = coverImgRes.secure_url;
+    }
+
+    user.username = username || user.username;
+    user.fullName = fullName || user.fullName;
+    user.email = email || user.email;
+    user.bio = bio || user.bio;
+    user.link = link || user.link;
+    user.profileImg = profileImg || user.profileImg;
+    user.coverImg = coverImg || user.coverImg;
+
+    user = await User.save();
+  } catch (err) {
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+};
 
 module.exports = {
   getUserProfile,
   followUnfollow,
   updateProfile,
-  suggestedUsers
+  suggestedUsers,
 };
--- Backend/server.js
+++ Backend/server.js
@@ -3,10 +3,21 @@ const app = express();
 const cors = require("cors");
 const connectDb = require("../Backend/db");
 const cookieParser = require("cookie-parser");
+const {v2} = require("cloudinary");
 const Port = 5000;
+const dotenv = require("dotenv");
+const cloudinary = v2;
 
 const authRoutes = require("./routes/auth");
 const userRoutes = require("./routes/user");
+
+dotenv.config()
+
+cloudinary.config({
+    cloud_name: process.env.CLOUD_NAME,
+    api_key: process.env.CLOUD_API,
+    cloud_secret: process.env.CLOUD_SECRET
+})
 
 app.use(cors());
 app.use(express.json()); // to parse req.body
