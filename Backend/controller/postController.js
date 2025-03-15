2007659437f8a02c6c5848d68b009b38c5d5ad48
Mohammad Dinar <web.dev.dinar@gmail.com>
2025-03-15 16:47:21 +0600

    Added commenting feature

--- Backend/controller/postController.js
+++ Backend/controller/postController.js
@@ -59,6 +59,37 @@ const deletePost = async (req, res) => {
     }
 };
 
+const commentOnPost = async (req, res) => {
+    try{
+        const {text} = req.body;
+		const postId = req.params.id;
+        const userId = req.user._id;
+
+        if(!text){
+            return res.status(404).json({
+             error: "Text field is required"
+            })
+        }
+
+        const post = await Post.findById(postId);
+
+		if (!post) return res.status(404).json({ error: "Post not found" });
+        
+        const comment = {
+            user: userId,
+            text
+        };
+
+		post.comments.push(comment);
+		await post.save();
+		res.status(200).json({post});
+
+    }catch(err){
+        res.status(500).json({error: "Internal Server Error"});
+        console.log(err)
+    }
+};
+
 module.exports = {
-    createPost, deletePost
+    createPost, deletePost, commentOnPost
 }
--- Backend/routes/post.js
+++ Backend/routes/post.js
@@ -1,9 +1,10 @@
 const express = require("express");
-const { createPost, deletePost } = require("../controller/postController");
+const { createPost, deletePost, commentOnPost } = require("../controller/postController");
 const protectedRoute = require("../middleware/protectedRoute");
 const router = express.Router();
 
 router.post("/create", protectedRoute, createPost);
+router.post("/comment/:id", protectedRoute, commentOnPost);
 router.delete("/:id", protectedRoute, deletePost);
 
 module.exports = router;
