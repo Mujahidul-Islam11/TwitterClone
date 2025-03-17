956cf83bfa077e9937a48302c1d718a080250d3b
Mohammad Dinar <web.dev.dinar@gmail.com>
2025-03-17 15:19:02 +0600

    Created ui design

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,11 +1,23 @@
+import { Route, Routes } from "react-router-dom"
+import SignUpPage from "./pages/auth/signup/SignUpPage"
+import LoginPage from "./pages/auth/login/LoginPage"
+import HomePage from "./pages/home/homePage"
+import Sidebar from "./components/common/Sidebar"
+import RightPanel from "./components/common/RightPanel"
+import ProfilePage from "./pages/proflie/ProfilePage"
 
 function App() {
 
   return (
-    <div>
-      <button className="btn btn-accent">Accent</button>
-      <button className="btn btn-info">Info</button>
-      <button className="btn btn-success">Success</button>
+    <div className='flex max-w-6xl mx-auto'>
+      <Sidebar />
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+        <Route path='/signup' element={<SignUpPage />} />
+        <Route path='/login' element={<LoginPage />} />
+        <Route path='/profile/:username' element={<ProfilePage />} />
+      </Routes>
+      <RightPanel />
     </div>
   )
 }
